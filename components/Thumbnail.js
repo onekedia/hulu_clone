528963b69edfd49b369c32f2a94fff16be3d8a2e
Goldie kedia <theone.kedia@gmail.com>
2021-12-21 18:52:53 +0530

    Added display name

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -13,6 +13,7 @@ const Thumbnail = forwardRef(({result}, ref) => {
                     }
                 height={1080}
                 width={1920}
+                alt={result.title}
                 />
             <div className='p-2'>
                 <p className="truncate max-w-md">{result.overview}</p>
@@ -26,5 +27,5 @@ const Thumbnail = forwardRef(({result}, ref) => {
         </div>
     )
 })
-
+Thumbnail.displayName = 'Thumbnail';
 export default Thumbnail
